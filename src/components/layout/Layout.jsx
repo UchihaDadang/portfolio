936f0a65b20191d30888c54778e3449b95f9c73e
yusri ganteng <yusri338458@gmail.com>
2025-09-22 21:40:56 +0700

    create a layout techstack

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../sections/Hero";
 import AboutSection from "../sections/About";
+import TechStack from "../sections/Skills";
 
 export default function Layouts() {
     return(
         <>
             <HeroSection/>
             <AboutSection/>
+            <TechStack/>
         </>
     )
 }
